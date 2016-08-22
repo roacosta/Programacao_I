7d3c6551e9d8154e9d1bc490edb042c7552c25ef
roacosta <digo_acosta@hotmail.com>
2016-08-22 20:49:04 -0300

    Conta

--- Conta.java
+++ Conta.java
@@ -1,8 +1,10 @@
-
+import java.util.Scanner;
 class Conta{
 	int 	numero;
 	String 	dono;
 	double 	saldo;
+
+	Scanner entrada = new Scanner (System.in);
 
 	boolean	saca(double valor){
 		if (this.saldo >= valor){
@@ -17,5 +19,12 @@ class Conta{
 	}
 	void	deposita(double valor){
 		this.saldo += valor;
+	}
+	void	criar(){
+			Conta minhaConta = new Conta();
+			System.out.println("\n\n Informe o numero da conta: ");		
+			minhaConta.dono = entrada.nextLine();	
+			System.out.println("\n\n Informe o Titular da conta: ");
+
 	}
 }
